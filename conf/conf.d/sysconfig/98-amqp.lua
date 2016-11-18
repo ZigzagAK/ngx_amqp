f22ea11a4dc90985181ca9398c52abf9a2d4aea2
Aleksey Konovkin <alkon2000@mail.ru>
2016-11-18 11:21:00 +0300

    add trace_ddl & trace_publish

--- conf/conf.d/sysconfig/98-amqp.lua
+++ conf/conf.d/sysconfig/98-amqp.lua
@@ -5,16 +5,17 @@ local _M = {
 local CONFIG = ngx.shared.config
 
 function _M.config()
-  CONFIG:set("amqp.async_queue_size", 1000)
-  CONFIG:set("amqp.publisher_pool_size", 1)
+  CONFIG:set("amqp.async_queue_size", 2000)
+  CONFIG:set("amqp.publisher_pool_size", 5)
   CONFIG:set("amqp.publisher_timeout", 1)
-  CONFIG:set("amqp.host", "127.0.0.1")
+  CONFIG:set("amqp.host", "192.168.2.12")
   CONFIG:set("amqp.port", 5672)
   CONFIG:set("amqp.user", "root")
   CONFIG:set("amqp.password", "1111")
   CONFIG:set("amqp.vhost", "/")
   CONFIG:set("amqp.ssl", false)
-  CONFIG:set("amqp.trace_messages", true)
+  CONFIG:set("amqp.trace_ddl", true)
+  CONFIG:set("amqp.trace_publish", false)
   CONFIG:set("amqp.no_wait", false)
 end
 
