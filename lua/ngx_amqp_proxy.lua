2b7f90ba35b6f31ac8dc2609ac6e2fc13b1a7c87
Aleksey Konovkin <alkon2000@mail.ru>
2016-11-21 10:51:15 +0300

    2 separate threads for up/down streams

--- lua/ngx_amqp_proxy.lua
+++ lua/ngx_amqp_proxy.lua
@@ -30,14 +30,14 @@ function _M.proxy()
       ngx.exit(444)
     end--]]
     
-    local server = "127.0.0.1:5672"
+    local server = "192.168.2.12:5672"
 
     local host, port = server:match("^([^:]+):([0-9]+)$")
 
-    local downstream_sock = ngx.req.socket()
-    local upstream_sock = ngx.socket.tcp()
+    local sock_down = ngx.req.socket()
+    local sock_up = ngx.socket.tcp()
 
-    local ok, err = upstream_sock:connect(host, tonumber(port))
+    local ok, err = sock_up:connect(host, tonumber(port))
     if not ok then
       ngx.log(ngx.ERR, "failed to connect: ", err)
       ngx.exit(444)
@@ -45,103 +45,60 @@ function _M.proxy()
 
     local mt = { __index = {
       receive = function(self, size)
-        local data, err = self.from:receive(size)
+        local data
+        data, self.err = self.from:receive(size)
         if data then
-          local bytes, err = self.to:send(data)
+          local bytes
+          bytes, self.err = self.to:send(data)
           if not bytes then
-            ngx.log(ngx.ERR, err)
-            ngx.exit(444)
+            return nil, self.err
           end
         end
-        return data, err
-      end,
-      send = function(self, data)
-        return self.from:send(data)
+        return data, self.err
       end
     } }
 
     local request = {
-      from = downstream_sock,
-      to = upstream_sock,      
+      from = sock_down,
+      to = sock_up,
     }
     local response = {
-      from = upstream_sock,
-      to = downstream_sock,      
+      from = sock_up,
+      to = sock_down,
     }
 
     setmetatable(request, mt)
     setmetatable(response, mt)
 
-    upstream_sock:settimeout(100)
-    downstream_sock:settimeout(100)
+    sock_up:settimeout(100)
+    sock_down:settimeout(100)
     
-    local conn_is_estabilished
-    local no_wait = {
-      [c.class.CONNECTION] = {
-        [c.method.connection.TUNE_OK] = true
-      },
-      [c.class.BASIC] = {
-        [c.method.basic.PUBLISH] = true
-      }
-    }
-    
-    while true
-    do
-:: continue ::
-      local f, err = frame.consume_frame( {
-        sock = request
-      } )
-
-      if not conn_is_estabilished then
-        conn_is_estabilished = true
-        goto consume
-      end
-    
-      if not f then
-        if err then
-          if err ~= "timeout" then
-            ngx.log(ngx.ERR, "amqp consume error : " .. err)
-            break
-          end
-          goto consume
+    local thr_func = function(ctx)
+      while not ctx.err or ctx.err == "timeout"
+      do
+        local f, err = frame.consume_frame( {
+          sock = ctx.sock
+        } )
+        if f then
+          ngx.log(ngx.INFO, "amqp " .. ctx.desc .. " : " .. cjson.encode(f))
+        elseif err and err ~= "timeout" then
+          ngx.log(ngx.WARN, "amqp " .. ctx.desc .. " : " .. (err or "?"))
         end
       end
-
-      ngx.log(ngx.INFO, "amqp request : " .. cjson.encode(f))
-  
-      if no_wait[f.class_id] and f.method_id and no_wait[f.class_id][f.method_id] then
-        goto continue
-      end
-
-      if f.class_id == c.class.BASIC then
-        if f.type == c.frame.HEADER_FRAME then
-          goto continue
-        end
-      end
-
-      if f.type == c.frame.BODY_FRAME then
-        goto continue
-      end
-
-:: consume ::
-      f, err = frame.consume_frame( {
-        sock = response
-      })
-
-      if not f then
-        if err then
-          if err ~= "timeout" then
-            ngx.log(ngx.ERR, "amqp consume error : " .. err)
-            break
-          end
-          goto continue
-        end
-      end
-      
-      ngx.log(ngx.INFO, "amqp response : " .. cjson.encode(f))    
     end
 
-    upstream_sock:close()
+    local thr_up = ngx.thread.spawn(thr_func, { sock = request, desc = "request" })
+    local thr_down = ngx.thread.spawn(thr_func, { sock = response, desc = "response" })
+
+    local ok, err = ngx.thread.wait(thr_up, thr_down)
+    if not ok then
+      ngx.log(ngx.ERR, "failed to wait: " .. err)
+    end
+
+    ngx.thread.kill(thr_up)
+    ngx.thread.kill(thr_down)
+  
+    sock_up:close()
 end
 
 return _M
