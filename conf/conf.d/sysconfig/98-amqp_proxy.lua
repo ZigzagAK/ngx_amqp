a3199a71618161ac1eb257c4a3703f61a4d9209f
Aleksey Konovkin <alkon2000@mail.ru>
2016-12-04 13:59:10 +0300

    change unix socket path

--- conf/conf.d/sysconfig/98-amqp_proxy.lua
+++ conf/conf.d/sysconfig/98-amqp_proxy.lua
@@ -6,7 +6,7 @@ local _M = {
 local CONFIG = ngx.shared.config_s
 
 function _M.config()
-  CONFIG:set("amqp_proxy.upstream_host", "unix:/tmp/nginx.amqp.sock")
+  CONFIG:set("amqp_proxy.upstream_host", "unix:logs/amqp.sock")
   CONFIG:set("amqp_proxy.upstream_port", nil)
   CONFIG:set("amqp_proxy.trace_ddl", true)
   CONFIG:set("amqp_proxy.trace_dml", true)
